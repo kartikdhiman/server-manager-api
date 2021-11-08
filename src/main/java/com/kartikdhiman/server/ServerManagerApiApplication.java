aeab77570733211781e40c93335f8851b71394d2
Kartik Dhiman <kartikjuly@gmail.com>
2021-11-08 19:41:06 +0530

    Initial commit

---
+++ src/main/java/com/kartikdhiman/server/ServerManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.kartikdhiman.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kartikdhiman/server/ServerManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartikdhiman.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
